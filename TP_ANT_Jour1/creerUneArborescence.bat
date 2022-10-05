42ca6f7fc928a5d87eb0b61ca252100ea67ef3e3
Adrien  Houen <103360104+AdHouen@users.noreply.github.com>
2022-10-05 21:56:50 +0200

    Add files via upload

---
+++ TP_ANT_Jour1/creerUneArborescence.bat
@@ -0,0 +1,23 @@
+@echo off
+
+mkdir I:\GitHub\TP_ANT\TP_ANT_20221003\TP_ANT_tp1\TP_DOS\R1
+
+copy nul I:\GitHub\TP_ANT\TP_ANT_20221003\TP_ANT_tp1\TP_DOS\R1\tata
+
+copy nul I:\GitHub\TP_ANT\TP_ANT_20221003\TP_ANT_tp1\TP_DOS\R1\ab12
+
+copy nul I:\GitHub\TP_ANT\TP_ANT_20221003\TP_ANT_tp1\TP_DOS\R1\tutu
+
+copy nul I:\GitHub\TP_ANT\TP_ANT_20221003\TP_ANT_tp1\TP_DOS\R1\titi
+
+copy nul I:\GitHub\TP_ANT\TP_ANT_20221003\TP_ANT_tp1\TP_DOS\R1\toto
+
+copy nul I:\GitHub\TP_ANT\TP_ANT_20221003\TP_ANT_tp1\TP_DOS\R1\trivial
+
+copy nul I:\GitHub\TP_ANT\TP_ANT_20221003\TP_ANT_tp1\TP_DOS\R1\t632
+
+copy nul I:\GitHub\TP_ANT\TP_ANT_20221003\TP_ANT_tp1\TP_DOS\R1\tbc2
+
+copy nul I:\GitHub\TP_ANT\TP_ANT_20221003\TP_ANT_tp1\TP_DOS\R1\tnt
+
+copy nul I:\GitHub\TP_ANT\TP_ANT_20221003\TP_ANT_tp1\TP_DOS\R1\t
